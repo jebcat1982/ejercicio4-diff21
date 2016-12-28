1b3812ff0ce9de5be7ac9cf034cca99e5d596e61
mariangelauribe <marita.91.up@gmail.com1>
2016-12-28 00:54:28 -0500

    agregando estilos y terminado

---
+++ main.js
@@ -0,0 +1,12 @@
+function calcular()
+{
+    var n = document.getElementById("numero").value;
+    var salida = document.getElementById("salida");
+    
+	if (n <= 21) {
+        salida.innerHTML = 21-n ;
+        
+    } else {
+        salida.innerHTML = (n-21)*2 ;
+    }
+}
